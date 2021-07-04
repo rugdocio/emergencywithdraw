b230e1d4ba7abc6dfdedcace55686f3188ce4508
Carlos Rodriguez <carlos-bsc@protonmail.com>
2021-07-04 03:41:27 +0200

    added bsc

--- src/components/Pools.js
+++ src/components/Pools.js
@@ -1,14 +1,25 @@
-import { List, Divider } from "antd";
+import { List, Divider, ConfigProvider } from "antd";
 import { useContext } from "react";
 import emergencyWithdraw from "../actions/emergencyWithdraw";
 import Web3Context from "../contexts/web3Context";
 import usePools from "../hooks/usePools";
+import { SmileOutlined } from '@ant-design/icons';
 
 import Pool from "./Pool";
+
+const customizeRenderEmpty = () => (
+  <div style={{ textAlign: 'center' }}>
+    <SmileOutlined style={{ fontSize: 20 }} />
+    <p>Provide a masterchef address to get started</p>
+    <p>Note: Make sure that you are connected to the correct network</p>
+  </div>
+);
+
 
 function Pools() {
   const pools = usePools();
   const { web3, chainId } = useContext(Web3Context);
+
   const onEmergencyWithdraw = async (item) => {
     const doEmergencyWithdraw = emergencyWithdraw(
       web3,
@@ -21,23 +32,26 @@ function Pools() {
   return (
     <>
       <Divider orientation="left">Pools</Divider>
-      <List
-        className="demo-loadmore-list"
-        size="large"
-        itemLayout="horizontal"
-        dataSource={pools}
-        renderItem={(item) => (
-          <Pool
-            name={item.name}
-            symbol={item.symbol}
-            amount={item.amount}
-            pid={item.pid}
-            chainId={chainId}
-            want={item.want}
-            onEmergencyWithdraw={() => onEmergencyWithdraw(item)}
-          ></Pool>
-        )}
-      />
+
+      <ConfigProvider renderEmpty={customizeRenderEmpty}>
+        <List
+          className="demo-loadmore-list"
+          size="large"
+          itemLayout="horizontal"
+          dataSource={pools}
+          renderItem={(item) => (
+            <Pool
+              name={item.name}
+              symbol={item.symbol}
+              amount={item.amount}
+              pid={item.pid}
+              chainId={chainId}
+              want={item.want}
+              onEmergencyWithdraw={() => onEmergencyWithdraw(item)}
+            ></Pool>
+          )}
+        />
+      </ConfigProvider>
     </>
   );
 }
--- src/config/constants/contracts.js
+++ src/config/constants/contracts.js
@@ -3,7 +3,7 @@
 const contracts = {
   fetcher: {
     137: "0x9BD3829315A2F076a7702885c3D0aE9184B76643",
-    56: "",
+    56: "0xB28C7D69Af6aed7a109FcAD522B7c81618f977A6",
   },
 };
 
