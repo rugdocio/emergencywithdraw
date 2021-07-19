b8c457175640c1413e7984b0d8c6d679f9be0869
Nate River <jambestwick@126.com>
2021-07-19 12:43:15 +0800

    update explorers
    add ftm ,avax  explorers

--- src/config/constants/explorers.js
+++ src/config/constants/explorers.js
@@ -4,6 +4,8 @@ export const explorers = {
       137: "https://polygonscan.com",
       56: "https://bscscan.com",
       321: "https://explorer.kcc.io/en",
+      250: "https://ftmscan.com",
+      43114: "https://avascan.info",
   };
 export const getAddressLink = (chainId, address) => {
     return explorers[chainId] + "/address/" + address
