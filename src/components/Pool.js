1be92a551a0ecc5037f5f95da8f4c590bac8f54b
carlos-bsc <carlos-bsc@protonmail.com>
2021-07-20 17:26:47 +0200

    added revoke button

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,4 +1,4 @@
-import { Avatar, List, Button } from "antd";
+import { Avatar, List, Button, Space } from "antd";
 import { getAddressLink } from "../config/constants/explorers";
 import { formatBigNumberToFixed } from "../utils/decimals";
 
@@ -6,14 +6,14 @@ import { formatBigNumberToFixed } from "../utils/decimals";
 // When the emergencyWithdraw button is pressed, the injected "onEmergencyWithdraw" method is called.
 //https://cryptologos.cc/logos/binance-usd-busd-logo.svg?v=010
 function Pool(props) {
-  const { name, symbol, amount, pid, chainId, want, depositFee, decimals, isFallback, onEmergencyWithdraw } = props; // consider restructuring this
-  let decAmount = formatBigNumberToFixed(amount, decimals,decimals);
-  if (decAmount > 1){
-    decAmount = formatBigNumberToFixed(amount, 2,decimals);
-  } else if (decAmount > 0.1){
-    decAmount = formatBigNumberToFixed(amount, 3,decimals);
-  }else if (decAmount > 0.001){
-    decAmount = formatBigNumberToFixed(amount, 5,decimals);
+  const { name, symbol, amount, pid, chainId, want, depositFee, decimals, isFallback, onEmergencyWithdraw, onRevoke } = props; // consider restructuring this
+  let decAmount = formatBigNumberToFixed(amount, decimals, decimals);
+  if (decAmount > 1) {
+    decAmount = formatBigNumberToFixed(amount, 2, decimals);
+  } else if (decAmount > 0.1) {
+    decAmount = formatBigNumberToFixed(amount, 3, decimals);
+  } else if (decAmount > 0.001) {
+    decAmount = formatBigNumberToFixed(amount, 5, decimals);
   }
 
   return (
@@ -23,15 +23,20 @@ function Pool(props) {
           <Avatar src="https://rugdoc.io/assets/placeholder.png" />
         }
 
-        title={<a target="_blank" rel="noreferrer" href={getAddressLink(chainId,want)}>{name} - PID: {pid} [ {(isFallback ? "??.??" :formatBigNumberToFixed(depositFee, 2,2))+ "%"} ]</a>}
+        title={<a target="_blank" rel="noreferrer" href={getAddressLink(chainId, want)}>{name} - PID: {pid} [ {(isFallback ? "??.??" : formatBigNumberToFixed(depositFee, 2, 2)) + "%"} ]</a>}
         description={(amount === "0" ? "0" : decAmount) + " " + symbol}
       />
 
       <div>
         {" "}
-        <Button type="primary" danger onClick={onEmergencyWithdraw}>
-          Emergency Withdraw
-        </Button>
+        <Space>
+          <Button type="primary" danger onClick={onEmergencyWithdraw}>
+            Emergency Withdraw
+          </Button>
+          <Button type="primary" onClick={onRevoke}>
+            Revoke
+          </Button>
+        </Space>
       </div>
     </List.Item>
   );
