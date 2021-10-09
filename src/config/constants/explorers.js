88b043eac6c07f94d93f04c268f8d309b08ecf51
carlos-bsc <carlos_bsc@protonmail.com>
2021-10-09 14:19:03 +0200

    added chains

--- src/config/constants/explorers.js
+++ src/config/constants/explorers.js
@@ -5,7 +5,11 @@ export const explorers = {
       56: "https://bscscan.com",
       321: "https://explorer.kcc.io/en",
       250: "https://ftmscan.com",
+      1285: "https://blockscout.moonriver.moonbeam.network/",
+      4689: "https://v2.iotexscan.io",
       43114: "https://avascan.info/blockchain/c",
+      42220: "https://explorer.celo.org",
+      1666600000: "https://explorer.harmony.one/",
   };
 export const getAddressLink = (chainId, address) => {
     return explorers[chainId] + "/address/" + address
